f9f064fa3f33214886be571eef878c7250592018
MD ABUL HOSSAIN <44331073+abul2285@users.noreply.github.com>
2021-02-08 12:00:03 +0600

    babel added to the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log({ hello: "hello" });
+const Student = {
+  class: 7,
+  age: 13,
+  gender: "male",
+};
+
+const S1 = {
+  ...Student,
+  name: "S1",
+};
+
+const S2 = {
+  ...Student,
+  name: "S2",
+};
+
+console.log({ S1, S2 });
